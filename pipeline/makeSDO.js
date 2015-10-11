9ff9ec8efb4650eb227b935aafa68c47408f0f5d
Dan Delany <dan.delany@gmail.com>
2015-10-11 15:36:13 -0400

    work on sdo script

--- pipeline/makeSDO.js
+++ pipeline/makeSDO.js
@@ -29,43 +29,99 @@ const imgPath = '/media/dan/Galactica/earth/img/sdo'
 // const outPath = '../testout/sdo';
 const outPath = '/media/dan/Galactica/earth/output/sdo'
 
+const dayURI = date => `${baseURI}/${dateToPath(date)}`;
+const dayImgPath = date => `${imgPath}/${dateToPath(date)}`;
+const bandImgPath = (date, band) => `${dayImgPath(date)}/${band}`;
+const dayOutPath = date => `${outPath}/${dateToPath(date)}`;
+const bandOutPath = (date, band) => `${dayOutPath(date)}/${band}`;
+
 const saveResolution = '4096';
-const saveBands = ['0131', '0171'];
-// const saveBands = ['0131']
+// const saveBands = ['0131', '0171'];
+const saveBands = ['0171']
 const crops = ['1920x1080+2096+2200'];
 
+const products = [
+    {
+        name: 'active-region-2015-10-01',
+        start: moment.utc('2015-09-28'),
+        days: 6,
+        bands: ['0171'],
+        crops: ['1920x1080+2096+2200'],
+        resolution: '4096'
+    }
+];
+
 function main(callback) {
-    const date = moment.utc('2015-10-01');
+    products.forEach(makeSDOProduct);
+
+    return;
+
+    const date = moment.utc('2015-09-28');
     console.log(dayURIFromDate(date));
-    // retrieveSDOImages(baseURI, date, {bands: saveBands, resolution: saveResolution}, () => {
-    //    cropSDOImages(date, saveBands, crops, outPath);
-    //    makeSDOVideo(date, saveBands, crops, outPath);
-    // });
-    cropSDOImages(date, saveBands, crops, outPath);
-    makeSDOVideo(date, saveBands, crops, outPath);
+    retrieveSDOImages(baseURI, date, saveBands, {resolution: saveResolution}, () => {
+       cropSDOImages(date, saveBands, crops, outPath);
+       makeSDOVideo([date], saveBands, crops, outPath);
+    });
+    // cropSDOImages(date, saveBands, crops, outPath);
+    // makeSDOVideo(date, saveBands, crops, outPath);
 }
+
+function makeSDOProduct(product, callback=_.noop) {
+    const q = queue({concurrency: 1});
+    const {start, days, bands, crops, resolution} = product;
+    const dates = _.range(days).map(i => start.clone().add(i, 'days'));
+    const productPath = `${outPath}/products/${product.name || 'untitled'}`;
+    const productImgPath = `${productPath}/img`;
+    const productVideoPath = `${productPath}/video`;
+
+    // for each day in this product, request all images for required bands
+    ensureDir(productImgPath);
+    dates.forEach(date => {
+        q.push(cb => retrieveSDOImages(date, bands, {resolution}, cb));
+        
+    });
+    dates.forEach(date => {
+        q.push(cb => { cropSDOImages(date, bands[0], crops, productImgPath); cb(); })
+    });
+
+    q.push(cb => makeSDOVideo(productImgPath, productVideoPath, product.name));
+
+    q.start(callback);
+}
+
 main();
 
-function retrieveSDOImages(baseURI, date, {bands=[], resolution='4096'}, callback=_.noop) {
-    const dayURI = dayURIFromDate(date);
-    const dayPath = `${imgPath}/${dateToPath(date)}`;
-    ensureDir(dayPath);
 
-    request(dayURI, (error, response, body) => {
-        const dayImgURIs = sdoImgURIsFromHtml(body, dayURI);
+function retrieveSDOImages(date, bands, {resolution='4096', expected=96}, callback=_.noop) {
+    // check if we already have all images for this day + bands & short circuit if so
+    const isDone = _.all(bands, band => dirHasJpgs(bandImgPath(date, band), expected));
+    if(isDone) { 
+        console.log(`already have images for ${dateToPath(date)}`); 
+        callback(); 
+        return;
+    } else console.log(`requesting image list for ${dateToPath(date)}...`)
+
+    // get the index page for this date and parse the URLs listed in it to get image details
+    const uri = dayURI(date);
+    request(uri, (error, response, body) => {
+        const dayImgURIs = sdoImgURIsFromHtml(body, uri);
         const dayImages = dayImgURIs
             .map(parseSDOURI)
             .filter(img => img.resolution === resolution);
+        console.log(`available AIA bands: ${_.uniq(_.pluck(dayImages, 'band'))}`);
 
+        // check each requested band
         bands.forEach(band => {
-            const bandPath = `${dayPath}/${band}`;
+            const bandPath = bandImgPath(date, band);
             const bandImages = dayImages.filter(img => img.band === band);
-            if(!bandImages.length) return;
-            ensureDir(bandPath);
+            if(!bandImages.length) { console.log(`no images for band ${band} ${date}`); return; }
+            if(dirHasJpgs(bandPath, expected)) { console.log(`already done with ${bandPath}`); return; }
 
+            // the server has images for this band that we don't have, so request them
+            ensureDir(bandPath);
             bandImages.forEach(img => {
                 const imgPath = `${bandPath}/${img.fileName}`;
-                if(fileExists(imgPath)) { console.log(`image exists at ${imgPath}`); return; }
+                if(fileExists(imgPath)) { console.log(`image exists for ${imgPath}`); return; }
                 execAndLog(wget(img.uri, imgPath), true, 'wget');
                 execAndLog(`sleep 1`);
             });
@@ -74,58 +130,82 @@ function retrieveSDOImages(baseURI, date, {bands=[], resolution='4096'}, callbac
     });
 }
 
-function cropSDOImages(date, bands, crops, outPath) {
+
+// function cropSDOImages(date, bands, crops) {
+//     // crop the images based on given coordinates
+//     bands.forEach(band => {
+//         const bandPath = bandImgPath(date, band);
+//         if(!dirHasJpgs(bandPath)) { console.log(`no images for ${dateToPath(date)} ${band}`); return; }
+
+//         crops.forEach(crop => {
+//             const cropDir = `${bandOutPath(date, band)}/${crop}/img`;
+//             const isDone = (sh.ls(bandPath).filter(isJpg).length === sh.ls(cropDir).filter(isJpg).length);
+//             if(isDone) { console.log(`crops exist for ${dateToPath(date)} ${band} ${crop}`); return; }
+
+//             // const mogrifyCmd = `mogrify -path ${cropDir} -crop ${crop} ${bandPath}/*.jpg`;
+//             const mogrifyCmd = `mogrify -path ${cropDir} -crop ${crop} `+
+//                 `-type Grayscale -contrast-stretch 0 -unsharp 0  ${bandPath}/*.jpg`;
+//             ensureDir(cropDir);
+//             execAndLog(mogrifyCmd, true, 'mogrify');
+//         })
+//     })
+// }
+
+function cropSDOImages(date, band, crop, cropDir) {
     // crop the images based on given coordinates
-    const dayPath = `${imgPath}/${dateToPath(date)}`;
-    if(!dirExists(dayPath)) return;
+    const bandPath = bandImgPath(date, band);
+    if(!dirHasJpgs(bandPath)) { console.log(`no images for ${dateToPath(date)} ${band}`); return; }
 
-    bands.forEach(band => {
-        const bandPath = `${dayPath}/${band}`;
-        if(!dirExists(bandPath)) return;
-        const imgFiles = sh.ls(bandPath);
-        if(!imgFiles.length) return;
-
-        crops.forEach(crop => {
-            const cropDir = `${outPath}/${dateToPath(date)}/${band}/${crop}/img`;
-            console.log(cropDir);
-            // const mogrifyCmd = `mogrify -path ${cropDir} -crop ${crop} ${bandPath}/*.jpg`;
-            const mogrifyCmd = `mogrify -path ${cropDir} -crop ${crop} `+
-                `-type Grayscale -contrast-stretch 0 -unsharp 0  ${bandPath}/*.jpg`;
-            ensureDir(cropDir);
-            execAndLog(mogrifyCmd, true, 'mogrify');
-        })
-    })
+    if(!cropDir) cropDir = `${bandOutPath(date, band)}/${crop}/img`;
+    const mogrifyCmd = `mogrify -path ${cropDir} -crop ${crop} `+
+        `-type Grayscale -contrast-stretch 0 -unsharp 0  ${bandPath}/*.jpg`;
+    ensureDir(cropDir);
+    execAndLog(mogrifyCmd, true, 'mogrify');
 }
 
-function makeSDOVideo(date, bands, crops, outPath) {
-    const dayPath = `${imgPath}/${dateToPath(date)}`;
-    if(!dirExists(dayPath)) return;
+// function makeSDOVideo(dates, bands, crops, outPath) {
+//     const dayPath = `${imgPath}/${dateToPath(date)}`;
+//     if(!dirExists(dayPath)) return;
 
-    bands.forEach(band => {
-        crops.forEach(crop => {
-            const cropDir = `${outPath}/${dateToPath(date)}/${band}/${crop}`;
-            const cropImgDir = `${cropDir}/img`;
-            const videoDir = `${cropDir}/video`;
-            const videoPath = `${videoDir}/${date.format('YYYY-MM-DD')}-${band}.mp4`;
-            const interpPath = `${videoDir}/${date.format('YYYY-MM-DD')}-${band}-interpolated.mp4`;
-            if(!dirExists(cropImgDir) || !sh.ls(cropImgDir).length) return;
+//     bands.forEach(band => {
+//         crops.forEach(crop => {
+//             const cropDir = `${outPath}/${dateToPath(date)}/${band}/${crop}`;
+//             const cropImgDir = `${cropDir}/img`;
+//             const videoDir = `${cropDir}/video`;
+//             const videoPath = `${videoDir}/${date.format('YYYY-MM-DD')}-${band}.mp4`;
+//             const interpPath = `${videoDir}/${date.format('YYYY-MM-DD')}-${band}-interpolated.mp4`;
+//             if(!dirExists(cropImgDir) || !sh.ls(cropImgDir).length) return;
 
-            ensureDir(videoDir);
-            execAndLog(makeVideoCmd(cropImgDir, 3, videoPath), true, 'video creation');
-            execAndLog(`butterflow -v -s full,spd=1 -r 60 -o ${interpPath} ${videoPath}`);
-        })
-    })
+//             ensureDir(videoDir);
+//             execAndLog(makeVideoCmd(cropImgDir, 3, videoPath), true, 'video creation');
+//             execAndLog(`butterflow -v -s full,spd=1 -r 60 -o ${interpPath} ${videoPath}`);
+//         })
+//     })
+// }
+
+function makeSDOVideo(imgDir, outDir, name, {origFPS=3, finalFPS=60, speed=1}={}) {
+    if(!dirExists(imgDir) || !sh.ls(imgDir).length) return;
+    const videoPath = `${outDir}/${name}-original.mp4`;
+    const interpPath = `${outDir}/${name}-interpolated.mp4`;
+    if(fileExists(interpPath)) { console.log(`already made video for ${outDir}`); return; }
+
+    ensureDir(outDir);
+    execAndLog(makeVideoCmd(imgDir, origFPS, videoPath), true, 'video creation');
+    execAndLog(
+        `butterflow -v -s full,spd=${speed} -r ${finalFPS} -o ${interpPath} ${videoPath}`,
+    true, 'butterflow');
 }
 
 
-function sdoImgURIsFromHtml(htmlText, dayURI) {
+
+function sdoImgURIsFromHtml(htmlText, uri) {
     const $ = cheerio.load(htmlText);
     return Array.from(
         $('a') // find links with known text on the index page
             .filter((i, el) => isJpg(fileNameFromURI($(el).attr('href'))))
             .map((i, el) => $(el).attr('href'))
     )
-    .map(imgURI => absoluteURI(dayURI, imgURI)); // convert to absolute URIs
+    .map(imgURI => absoluteURI(uri, imgURI)); // convert to absolute URIs
 }
 
 function parseSDOURI(uri) {
@@ -147,3 +227,6 @@ function dateToPath(date) { return date.format('YYYY/MM/DD'); }
 
 function isJpg(name) { return name.indexOf('.jpg') > -1; }
 
+function dirHasJpgs(path, expected=1) {
+    return dirExists(path) && sh.ls(path).filter(isJpg).length >= expected;
+}
